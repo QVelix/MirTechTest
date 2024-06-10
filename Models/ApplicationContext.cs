16ff33ef3109bd766b4269bcc47b4affaee29aac
MaximBogachev <m.bogachev.prog@solo-it.ru>
2024-06-10 23:35:08 +0400

    Создал контекст бд

---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MirTechTest.Models;
+
+public class ApplicationContext : DbContext
+{
+	public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+	{
+		Database.EnsureCreated();
+	}
+	
+	public DbSet<Staff> Staffs { get; set; }
+}
