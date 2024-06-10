c8298311b6fb14be8f920b145c69ceb43c21f6d9
MaximBogachev <m.bogachev.prog@solo-it.ru>
2024-06-10 21:08:09 +0400

    Создал модель для сотрудников

---
+++ Models/Staff.cs
@@ -0,0 +1,10 @@
+﻿namespace MirTechTest.Models;
+
+public class Staff
+{
+	public string Department { get; set; }
+	public string FullName { get; set; }
+	public DateOnly BirthDay { get; set; }
+	public DateOnly DateOfEmployment { get; set; }
+	public int Wages { get; set; }
+}
