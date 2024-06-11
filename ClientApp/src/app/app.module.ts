c9022d142542f0d2ab91ef2abfe34430cbaac98f
MaximBogachev <m.bogachev.prog@solo-it.ru>
2024-06-11 04:14:19 +0400

    Удалил шаблонные файлы и изменил главную страницу, а так же верхнее меню

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import {StaffComponent} from "./staff/staff.component";
 
 @NgModule({
@@ -16,8 +14,6 @@ import {StaffComponent} from "./staff/staff.component";
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     StaffComponent
   ],
   imports: [
@@ -26,8 +22,6 @@ import {StaffComponent} from "./staff/staff.component";
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'staff', component: StaffComponent }
     ])
   ],
