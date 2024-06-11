2ad7e4fb11a2f3cc78926eab6f5458373daaa12f
MaximBogachev <m.bogachev.prog@solo-it.ru>
2024-06-11 04:08:38 +0400

    Дописал компонент сотрудников для корректного вывода кнопок и модального окна

--- ClientApp/src/app/staff/staff.component.ts
+++ ClientApp/src/app/staff/staff.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {MatDialog} from "@angular/material/dialog";
+import {StaffModalComponent} from "../staff-modal/staff-modal.component";
+import {IStaff} from "../Interfaces/IStaff";
 
 @Component({
   selector: 'app-staff',
@@ -6,5 +10,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./staff.component.css']
 })
 export class StaffComponent {
+  public staffs: IStaff[] = [];
 
+  constructor(public http: HttpClient, @Inject('BASE_URL') public baseUrl: string, public dialog: MatDialog) {
+    http.get<IStaff[]>(baseUrl + 'staff').subscribe(result => {
+      this.staffs = result;
+    }, error => console.error(error));
+  }
+
+  onClickOpenModal(status:string, selectedItem:IStaff|null){
+    let staff: IStaff|null = selectedItem;
+    const dialogRef = this.dialog.open(StaffModalComponent, {
+      width: '250px',
+      data: staff
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(status=='post'){
+        this.http.post(this.baseUrl+"staff", result).subscribe(answer=>{
+          this.staffs.push(result);
+        }, error=>{console.error(error)})
+      }
+      if(status=='put'){
+        this.http.post(this.baseUrl+"staff", result).subscribe(answer=>{
+          this.staffs.push(result);
+        }, error=>{console.error(error)})
+      }
+    });
+  }
 }
